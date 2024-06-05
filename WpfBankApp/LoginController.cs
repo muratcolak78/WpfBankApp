503e1256af5b88bf8f9176858d481fde246ded91
murat colak <colakmurat-78@hotmail.com>
2024-06-05 14:05:41 +0200

    loginController

---
+++ WpfBankApp/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBankApp
+{
+    public static class LoginController
+    {
+
+        public static bool loginControll(string userName, string passWort)
+        {
+
+
+            List<User> list = JSONHelper.readFromJson<User>("users.json");
+
+            AlleListen.User = AlleListen.Users.Find(n => n.username == userName && n.password == passWort);
+
+
+            AlleListen.User = AlleListen.Users.Find(n => n.username == userName && n.password == passWort);
+
+
+
+            if (AlleListen.User == null) return false;
+
+            return true;
+        }
+    }
+}

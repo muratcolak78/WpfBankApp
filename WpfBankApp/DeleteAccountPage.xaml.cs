529b4d96e7ad800617e901b19d9ef1ad392d2a32
murat colak <colakmurat-78@hotmail.com>
2024-06-05 13:58:04 +0200

    Deleteacoountcs hinzugefügt

--- WpfBankApp/DeleteAccountPage.xaml.cs
+++ WpfBankApp/DeleteAccountPage.xaml.cs
@@ -17,11 +17,47 @@ namespace WpfBankApp
     /// <summary>
     /// Interaktionslogik für DeleteAccountPage.xaml
     /// </summary>
-    public partial class DeleteAccountPage : Window
+    public DeleteAccountPage()
     {
-        public DeleteAccountPage()
+        InitializeComponent();
+        DataGridAllAccount.ItemsSource = AlleListen.User.Accounts;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var result = MessageBox.Show("The account will be deleted?", "Are you sure?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+        if (result.ToString() == "Yes")
         {
-            InitializeComponent();
+            if (AlleListen.Ibans.Contains(txtbxDeleteIban.Text))
+            {
+                try
+                {
+                    var deletedString = AlleListen.User.Accounts.Find(n => n.IBAN == txtbxDeleteIban.Text);
+                    AlleListen.User.Accounts.Remove(deletedString);
+                    MessageBox.Show("Account deleted");
+
+                    AlleListen.Ibans.Remove(deletedString.IBAN);
+
+                    JSONHelper.saveAsJson(AlleListen.Users, "users.json");
+                    JSONHelper.saveAsJson(AlleListen.Ibans, "ibans.json");
+                    txtbxDeleteIban.Text = string.Empty;
+
+                    AlleListen.aktullenGridData(DataGridAllAccount);
+
+                    //DataGridAllAccount.ItemsSource = null;
+                    //DataGridAllAccount.ItemsSource = AlleListen.User.Accounts;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(" Error while delteting account " + ex.Message);
+                }
+            }
+            else MessageBox.Show("There is no such account, Please check the IBAN numbers  ");
+
+
         }
+
     }
 }

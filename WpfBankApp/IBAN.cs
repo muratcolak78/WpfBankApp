350e1ea3d77f2cccb0c984674df87014d7f0ae47
murat colak <colakmurat-78@hotmail.com>
2024-06-05 14:00:38 +0200

    IBAN Klasse hinzugefügt

---
+++ WpfBankApp/IBAN.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBankApp
+{
+    internal static class IBAN
+    {
+        public static string ibanGenerator()
+        {
+            string iban = "DE";
+            bool exit = false;
+            Random random = new Random();
+            do
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    iban += random.Next(0, 10).ToString();
+                }
+                if (AlleListen.Ibans.Count == 0)
+                {
+
+                    exit = true;
+                }
+                else
+                {
+                    if (!AlleListen.Ibans.Contains(iban)) exit = true;
+                    else exit = false;
+                }
+
+            } while (!exit);
+            AlleListen.Ibans.Add(iban);
+            JSONHelper.saveAsJson<string>(AlleListen.Ibans, "ibans.json");
+            return iban;
+        }
+    }
+}

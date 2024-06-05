0398a1e24836825fa5bd00ee67eb683c78fe502f
murat colak <colakmurat-78@hotmail.com>
2024-06-05 13:59:40 +0200

    DepositPage hinzugefügt

---
+++ WpfBankApp/DepositPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfBankApp
+{
+    /// <summary>
+    /// Interaktionslogik für DepositPage.xaml
+    /// </summary>
+    public partial class DepositPage : Window
+    {
+        public DepositPage()
+        {
+            InitializeComponent();
+            DataGridAllAccount.ItemsSource = AlleListen.User.Accounts;
+
+        }
+
+        private void btnDeposit_Click(object sender, RoutedEventArgs e)
+        {
+            var account = AlleListen.User.Accounts.Find(n => n.IBAN == txtbxIBAN.Text);
+
+
+            if (account != null)
+            {
+                if (double.TryParse(txtbxBalance.Text, out double deposit))
+                {
+                    account.Deposit(deposit);
+
+                    JSONHelper.saveAsJson(AlleListen.Users, "users.json");
+                    //AlleListen.aktullenListen();
+
+                    AlleListen.aktullenGridData(DataGridAllAccount);
+
+                    MessageBox.Show("money deposited into account");
+                    txtbxIBAN.Text = string.Empty;
+                    txtbxBalance.Text = string.Empty;
+                }
+                else MessageBox.Show("please enter a valid numbers");
+            }
+            else MessageBox.Show("there is no such account");
+        }
+    }
+}

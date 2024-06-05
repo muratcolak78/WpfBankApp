98cf5bc5c6e86f769d04d41bef28e591f0ff70c9
murat colak <colakmurat-78@hotmail.com>
2024-06-05 14:07:39 +0200

    MainWindow

--- WpfBankApp/MainWindow.xaml.cs
+++ WpfBankApp/MainWindow.xaml.cs
@@ -19,6 +19,37 @@ namespace WpfBankApp
         public MainWindow()
         {
             InitializeComponent();
+
+            AlleListen.aktullenListen();
+
+        }
+
+        private void btnRegistrieren_Click(object sender, RoutedEventArgs e)
+        {
+            new RegistrierenFenster().Show();
+            this.Close();
+        }
+
+        private void btnAnmelden_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+            AlleListen.aktullenListen();
+
+
+            if (LoginController.loginControll(txtbxBenutzername.Text, txtbxPassword.Password))
+            {
+                new UserFenster().Show();
+                this.Close();
+            }
+            else MessageBox.Show("Ungültig benutzername oder passwort.\nbitte prüfenSie wieder");
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
         }
     }
 }
